d2841b7d6181e9df419faff9db2bbbc8da838701
nzommmo <ericnzomo17@gmail.com>
2024-08-18 22:41:08 +0300

    Update

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,9 +1,17 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
+import Home from "./Components/Homepage/Home"
 import Dashboard from "./components/dashboard"
 const App = () => {
   return (
-   <>
-   <Dashboard/>
-   </>
+   <Router>
+    <Routes>
+      <Route path="/" element={<Home/>}/>
+      <Route path="/dashboard" element={<Dashboard/>}/>
+
+
+    </Routes>
+   </Router>
+
   )
 }
 

bbee1b9cdf348aeaf6c807d85859602f1152cf31
nzommmo <ericnzomo17@gmail.com>
2024-08-20 12:56:18 +0300

    Update

--- Frontend/src/Components/Homepage/Hero.jsx
+++ Frontend/src/Components/Homepage/Hero.jsx
@@ -3,9 +3,9 @@ import React from 'react'
 const Hero = () => {
   return (
     <div className='ms-3 mt-10'>
-    <div className='flex   gap-4'>
+    <div className='flex   gap-2'>
      
-    <div className='bg-custom-image1 sm:mt-10 bg-contain bg-no-repeat md:h-[500px] md:w-[800px] sm:w-[400px] rounded-tl-lg ms-2'>
+    <div className='bg-custom-image1 sm:mt-10 bg-contain bg-no-repeat md:h-[500px] md:w-[800px] sm:h-[400px] sm:w-[400px] rounded-tl-lg ms-2'>
     </div>
 
     <div className='bg-customx py-3 px-3 h-full sm:mt-3 md:mt-20 md:me-2 rounded-tr-lg bg-opacity-30 rounded-bl-lg'>

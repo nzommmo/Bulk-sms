7a508434f67a596283150e6c105a4524a84a66a8
nzommmo <ericnzomo17@gmail.com>
2024-08-26 16:36:36 +0300

    Update

--- Frontend/src/Components/Homepage/WaitlistForm.jsx
+++ Frontend/src/Components/Homepage/WaitlistForm.jsx
@@ -6,19 +6,19 @@ const WaitlistForm = ({isVisible,onClose}) => {
   return (
     <div id="wrapper" className='fixed inset-0 bg-black z-10 bg-opacity-25 backdrop-blur-sm flex justify-center items-center'>
           <div className='w-[400px] '>
-          <div className='bg-black text-white h-[300px]  p-2 rounded flex flex-col my-0 pt-'>
+          <div className='bg-customl text-white h-[300px]  p-2 rounded-md flex flex-col my-0 pt-'>
 
-            <button className='text-Customl text-xl place-self-end z-50' onClick={onClose}>
+            <button className='text-customm font-bold text-xl place-self-end z-50' onClick={onClose}>
               X
             </button>
             <div>
-                <form action="" className='flex flex-col gap-5 mb-4 items-center justify-center'>
-                    <input type="text" placeholder='First Name' className='placeholder:px-2' />
+                <form action="" className='flex flex-col gap-5 mb-4 items-center justify-center rounded-md'>
+                    <input type="text" placeholder='First Name' className='px-3 rounded-md py-1 outline-none focus:outline-customm enabled:text-black bg-customx placeholder:text-customm' />
 
-                    <input type="text" placeholder='Last Name 'className='placeholder:px-2'/>
-                    <input type="email" placeholder='Email'className='placeholder:px-2'/>
+                    <input type="text" placeholder='Last Name 'className='px-3 outline-none focus:outline-customm enabled:text-black rounded-md py-1 bg-customx placeholder:text-customm'/>
+                    <input type="email" placeholder='Email'className=' outline-none focus:outline-customm enabled:text-black px-3 rounded-md py-1 bg-customx placeholder:text-customm'/>
 
-                    <button type="button"className='bg-customl mt-10 text-customm text-xl px-8 rounded-md'>Submit</button>
+                    <button type="button"className='bg-customm mt-10 text-customl text-xl px-8 rounded-md hover:bg-customg hover:text-customq'>Submit</button>
                 </form>
             </div>
             </div>

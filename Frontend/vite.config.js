1d41ff89c310071316b57d19fda8435c1c5abfa5
nzommmo <ericnzomo17@gmail.com>
2024-08-26 14:14:12 +0300

    Trial

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -1,15 +1,7 @@
-import path from "path";
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  build: {
-    outDir: 'dist', // Ensures that Vite outputs the build files to the 'dist' directory
-  },
-  plugins: [react()], // Includes the React plugin
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "src"), // Allows for importing from '@' as an alias for 'src'
-    },
-  },
-});
+  plugins: [react()],
+})
--- TEST/src/App.jsx
+++ TEST/src/App.jsx
@@ -1,10 +1,12 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
 import Home from "./Components/Homepage/Home"
+import Dashboard from "./components/dashboard"
 const App = () => {
   return (
    <Router>
     <Routes>
       <Route path="/" element={<Home/>}/>
+      <Route path="/dashboard" element={<Dashboard/>}/>
 
 
     </Routes>
